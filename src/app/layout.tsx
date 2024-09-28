81413e845ae30a4a7fa1b289762afe328ad372dd
Matt <director@acidgambit.com>
2024-09-28 10:14:55 -0400

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Footer } from "./_components/footer";
 import { Navbar } from "./_components/navbar";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} p-2 dark`}>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
